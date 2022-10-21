000b4860e3f4deeed59a2fcc8a9a49b496998893
leonardo-meneses <leonardo.rodrigounb@gmail.com>
2022-10-21 17:29:09 -0300

    Adicionando exceções.

---
+++ src/boardGame/BoardExcepition.java
@@ -0,0 +1,12 @@
+package boardGame;
+
+public class BoardExcepition extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+
+	public BoardExcepition(String string) {
+		// TODO Auto-generated constructor stub
+		super(string);
+	}
+
+}
